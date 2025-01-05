a20f8f6955dc33f19bb362d6c42f7ad62afb88ae
pray3m <prem.gtm9@gmail.com>
2025-01-05 23:09:46 +0545

    Exericse 9.12: Patientor backend, step5

--- part9/patientor/backend/src/routes/patients.ts
+++ part9/patientor/backend/src/routes/patients.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Response, Router } from "express";
 import patientsService from "../services/patientsService";
 import { PublicPatient } from "../types";
@@ -7,6 +8,18 @@ const router = Router();
 router.get("/", (_req, res: Response<PublicPatient[]>) => {
   const patients = patientsService.getEntries();
   res.status(200).json(patients);
+});
+
+router.post("/", (req, res) => {
+  const { dateOfBirth, gender, name, occupation, ssn } = req.body;
+  const addedPatient = patientsService.addPatient(
+    dateOfBirth,
+    gender,
+    name,
+    occupation,
+    ssn
+  );
+  res.json(addedPatient);
 });
 
 export default router;
--- part9/patientor/backend/src/services/patientsService.ts
+++ part9/patientor/backend/src/services/patientsService.ts
@@ -1,5 +1,8 @@
+import { v1 as uuid } from "uuid";
 import patients from "../../data/patients";
 import { PublicPatient } from "../types";
+
+const id = uuid();
 
 const getEntries = (): PublicPatient[] => {
   return patients.map(({ dateOfBirth, gender, id, name, occupation }) => ({
@@ -11,8 +14,23 @@ const getEntries = (): PublicPatient[] => {
   }));
 };
 
-const addPatients = () => {
-  return null;
+const addPatient = (
+  dateOfBirth: string,
+  gender: string,
+  name: string,
+  occupation: string,
+  ssn: string
+): PublicPatient => {
+  const newPatient = {
+    id,
+    dateOfBirth,
+    gender,
+    name,
+    occupation,
+    ssn,
+  };
+  patients.push(newPatient);
+  return newPatient;
 };
 
-export default { getEntries, addPatients };
+export default { getEntries, addPatient };
